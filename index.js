3d2a671a395f077d3a013079d2378422cac4d3f6
rauleflores <58542850+rauleflores@users.noreply.github.com>
2020-03-19 17:34:26 -0700

    finished task 3

--- index.js
+++ index.js
@@ -109,13 +109,13 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-  constructor(name, age, location){
-    this.name = name;
-    this.age = age;
-    this.location = location;
+  constructor(options){
+    this.name = options.name;
+    this.age = options.age;
+    this.location = options.location;
   }
   speak(){
-    
+    return `Hello my name is ${this.name}, I am from ${this.location}`
   }
 }
 
